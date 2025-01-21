f778ddfe84c99b5a43f8d262c3defcd3746b24fe
cloudroller <adamleesr4@gmail.com>
2025-01-21 18:37:21 +0100

    Update access.go

--- access.go
+++ access.go
@@ -1,7 +1,6 @@
-// credentials.go
-package azureopenai_credentials_tester
+package azure_openai
 
-// Test credentials for Azure OpenAI
+// Azure OpenAI API Key and Endpoint
 const (
 	AzureOpenAIKey        = "9apB9EtnpbOJYqPS0vzEbr8T976woI5FYLtnN2HdnTRkQymT5lZNJQQJ99AKACYeBjFXJ3w3AAABACOGFyYH"
 	AzureOpenAIEndpoint   = "https://aws.openai.azure.com/"
